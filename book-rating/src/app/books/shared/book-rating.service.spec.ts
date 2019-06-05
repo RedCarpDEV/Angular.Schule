ef9b4484ee517d52471d111ad8f57ede3975bc25
Timo Wamser <t.wamser@outlook.de>
2019-06-05 13:30:22 +0200

    ersete tests

---
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -0,0 +1,50 @@
+import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
+
+fdescribe('BookRatingService', () => {
+
+  let service: BookRatingService;
+
+  let book: Book;
+
+  beforeEach(() => {
+
+    service = new BookRatingService();
+
+    book = {
+      isbn: '111',
+      title: 'Angular',
+      description: 'keine',
+      rating: 3
+    };
+
+  });
+
+  it('[JIRA-222] should rate up a book by one', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
+  });
+
+  it('[JIRA-223] should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should always return a new book instance (assuming immutablity)', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook).not.toBe(book);
+  });
+
+});
---
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookRatingService {
+
+  readonly minRating = 1;
+  readonly maxRating = 5;
+
+  rateUp(book: Book) {
+    return {
+      ...book,
+      rating: book.rating < this.maxRating ? book.rating + 1 : book.rating
+    };
+  }
+
+  rateDown(book: Book) {
+    return {
+      ...book,
+      rating: Math.max(book.rating - 1, this.minRating)
+    };
+  }
+
+}
