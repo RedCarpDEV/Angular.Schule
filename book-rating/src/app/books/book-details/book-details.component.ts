836eb913e569fdecdaf3acd4482f712955a8a9cf
Timo Wamser <t.wamser@outlook.de>
2019-06-06 15:23:57 +0200

    routing

---
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
