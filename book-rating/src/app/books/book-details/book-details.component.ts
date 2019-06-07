43d249899e796f681dbc161fe9142d70ac7969dd
Timo Wamser <t.wamser@outlook.de>
2019-06-07 14:05:50 +0200

    share... is aber late

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of, from, observable, Subscriber } from 'rxjs';
-import { map, switchMap, retry, filter, scan, reduce } from 'rxjs/operators';
+import { map, switchMap, retry, filter, scan, reduce, catchError, share } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 import { stringify } from 'querystring';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'br-book-details',
@@ -15,47 +16,26 @@ export class BookDetailsComponent implements OnInit {
 
   book$: Observable<Book>;
 
+  showDescription = false;
+
   constructor(private route: ActivatedRoute, private bookStoreService: BookStoreService) { }
 
   ngOnInit() {
-    /*
-    this.book$ = this.route.paramMap
+   this.book$ =
+    this.route.paramMap
       .pipe(
         map(paramMap => paramMap.get('isbn')),
-        switchMap(isbn => this.bookStoreService.getSingle(isbn)),
-        retry(3)
-      );
-      */
-
-
-    const observer = {
-      next: x => console.log(x),
-      error: err => console.error(err),
-      complete: () => console.log('Ende')
-    };
-
-    const myObservable$ = new Observable<number>(subscriber => {
-      subscriber.next(1);
-      subscriber.next(2);
-
-      setTimeout(() => subscriber.next(3), 1000);
-      setTimeout(() => subscriber.next(4), 1000);
-      setTimeout(() => subscriber.complete(), 2000);
-    });
-
-    const subscription = myObservable$
-      .pipe(
-        map(o => o * 10),
-        filter(el => el > 10),
-        reduce((x, y) => x + y),
-        map(x => '❤'.repeat(x))
+        switchMap(isbn => this.bookStoreService.getSingle(isbn)
+          .pipe(catchError((err: HttpErrorResponse) => of({
+            title: 'Error loading ' + err.url,
+            isbn: '111',
+            description: '...',
+            rating: 0
+          })))),
+          share()
       )
-      .subscribe(observer);
-
-
-
-
-    setTimeout(() => subscription.unsubscribe(), 3000);
+      // .subscribe(c => this.book = c)
+      ;
   }
 
 }
